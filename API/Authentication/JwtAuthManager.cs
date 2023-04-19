2029c9dbbb2031bbea6097f42c71ce247b7368de
Lee Zafra <leezafra11@gmail.com>
2023-04-19 23:50:50 +0800

    AES Encryption

---
+++ API/Authentication/JwtAuthManager.cs
@@ -0,0 +1,53 @@
+﻿using API.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Authentication
+{
+
+    public class JwtAuthManager
+    {
+        public IConfiguration _configuration;
+        public JwtAuthManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public String GenerateJwtToken(UserLogin user) {
+            
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = Encoding.ASCII.GetBytes
+            (_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", Guid.NewGuid().ToString()),
+                    new Claim("Username", user.Username),
+                    new Claim("Lastname", user.Lastname),
+                    new Claim(ClaimTypes.Role, user.Lastname),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+
+
+
+            return jwtToken;
+        }
+
+        
+    }
+}
--- API/Controllers/LoginController.cs
+++ API/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using API.Data;
+﻿using API.Authentication;
+using API.Data;
 using API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -19,13 +20,11 @@ namespace API.Controllers
     {
 
         private readonly IntellicareContext _context;
-        private readonly JwtSettings jwtSettings;
         public IConfiguration _configuration;
 
-        public LoginController(IConfiguration configuration, IOptions<JwtSettings> option, IntellicareContext context)
+        public LoginController(IConfiguration configuration, IntellicareContext context)
         {
             _context = context;
-            this.jwtSettings = option.Value;
             _configuration = configuration;
         }
 
@@ -53,82 +52,15 @@ namespace API.Controllers
                 return Ok(new { status = 401, isSuccess = false, message = "Incorrect Username or Password", });
             }
 
-            var tokenhandler = new JwtSecurityTokenHandler();
-            var tokenKey = Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value);
-            //var claims = new[] {
-            //                    //new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
-            //                    //new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            //                    //new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-            //                    new Claim("UserId", user.Id.ToString()),
-            //                    new Claim("DisplayName", user.Lastname),
-            //                    new Claim("UserName", user.Username),
-            //                    //new Claim("Email", user.Email)
-            //                };
-            var claim = new Claim[] {
-                new Claim(ClaimTypes.Name, user.Id.ToString()),
-                //new Claim(ClaimTypes.Name, user.Lastname)
-            };
+            var jwtauth = new JwtAuthManager(_configuration);
+            var token = jwtauth.GenerateJwtToken(user);
 
-            var tokendesc = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claim),
+            return Ok(token);
 
-                //Subject = new ClaimsIdentity(claim),
-                Expires = DateTime.Now.AddSeconds(20),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256)
-                    
-            };
-
-            var token = tokenhandler.CreateToken(tokendesc);
-            string finalToken = tokenhandler.WriteToken(token);
-
-
-            return Ok(new { status = 200, isSuccess = true, token = finalToken, message = "User Login successfully", UserDetails = user });
+            //return Ok(new { status = 200, isSuccess = true, token = finalToken, message = "User Login successfully", UserDetails = user });
             //return Ok(finalToken);
         }
 
-        //[HttpPost]
-        //[Route("Auth")]
-        //public async Task<IActionResult> AuthToken(Login? userLog)
-        //{
-        //    if (userLog != null && userLog.username != null && userLog.password != null)
-        //    {
-        //        UserLogin? user = await GetUser(userLog.username, userLog.password);
-
-        //        if (user != null)
-        //        {
-        //            //create claims details based on the user information
-        //            var claims = new[] {
-        //                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
-        //                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-        //                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-        //                new Claim("UserId", user.Id.ToString()),
-        //                new Claim("DisplayName", user.Lastname),
-        //                new Claim("UserName", user.Username),
-        //                //new Claim("Email", user.Email)
-        //            };
-
-        //            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-        //            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        //            var token = new JwtSecurityToken(
-        //                _configuration["Jwt:Issuer"],
-        //                _configuration["Jwt:Audience"],
-        //                claims,
-        //                expires: DateTime.UtcNow.AddMinutes(10),
-        //                signingCredentials: signIn);
-
-        //            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
-        //        }
-        //        else
-        //        {
-        //            return BadRequest("Invalid credentials");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return BadRequest();
-        //    }
-        //}
         private async Task<UserLogin> GetUser(string username, string password)
         {
 
